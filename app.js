6342d64ae2f1c9e926c0cfe7bf278db784fe8eaf
lyannel <ll3325@nyu.edu>
2019-05-01 11:17:05 -0400

    updated both client and server sides

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+// Setup basic express server
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
 app.use(express.static('public'));
-// server code goes here!
-// first listen for connection using io.on
-// then... within callback, use socket.on, socket.emit, socket.broadcast, etc.
-// NOTE THAT WE ARE LISTENING WITH server, NOT app!
-server.listen(3000);
+
+
+let numUsers = 0;
+io.on('connection', function (socket) {
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('move tears', function (data) {
+    console.log('Emitting tears');
+    socket.broadcast.emit('move tears', data);
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('move screaming', function (data) {
+    console.log('Emitting screams');
+    socket.broadcast.emit('move screaming', data);
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    console.log('disconnected!!');
+  });
+});
--- public/racer.js
+++ public/racer.js
@@ -1,1 +1,42 @@
- const socket = io();
+/* global io */
+
+  // Initialize variables
+document.addEventListener('DOMContentLoaded', function () {
+
+
+  const socket = io();
+  let left1 = 0
+  // Click events
+  document.querySelector('.player1Btn').addEventListener('click', function (evt) {
+    left1++;
+    console.log('What??');
+    socket.emit('move tears', {left: left1});
+  });
+
+  let left2 = 0
+  // Click events
+  document.querySelector('.player2Btn').addEventListener('click', function (evt) {
+    left2++;
+    console.log('Hello??');
+    socket.emit('move screaming', {left: left2});
+  });
+
+  // Socket events
+
+  // Whenever the server emits 'typing', show the typing message
+  socket.on('move tears', function (data) {
+    alert(data.left);
+    const player1 = document.querySelector('.player1');
+    console.log(player1);
+    player1.style.left = data.left+'px';
+
+  });
+
+  // Whenever the server emits 'stop typing', kill the typing message
+  socket.on('move screaming', function (data) {
+    alert(data.left);
+    const player2 = document.querySelector('.player2');
+    console.log(player2);
+    player2.style.left = data.left+'px';
+  });
+});
